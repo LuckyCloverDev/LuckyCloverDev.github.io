8e381c0aaa34a37a7c56f79e88467ef3fbfa4c54
LuckyCloverDev <technobuilder2003@gmail.com>
2023-08-01 22:26:22 -0700

    Added comic page for Cafe Dot

--- src/js/main-script.js
+++ src/js/main-script.js
@@ -1,12 +1,28 @@
-let frameButtons = document.getElementsByClassName("");
+var slideIndex = 1;
+showDivs(slideIndex);
 
-document.onload(() => {
-	console.log("READY");
-	frameButtons.forEach(btn => {
-		console.log(btn);
-	});
-})
+function plusDivs(n) {
+	showDivs(slideIndex += n);
+}
 
+function currentDiv(n) {
+	showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+	let i;
+	let x = document.getElementsByClassName("mySlides");
+
+	if (n > x.length) {slideIndex = 1}
+	if (n < 1) {slideIndex = x.length}
+	for (i = 0; i < x.length; i++) {
+    	x[i].style.display = "none";
+	}
+
+	console.log(x);
+	console.log(x[slideIndex-1]);
+	x[slideIndex-1].style.display = "block";
+}
 
 
 
